299ea5746a22b9b70a4bac9dd60ace09bb3581e1
Chetan Honnenahalli <chetan@liftoffllc.com>
2015-09-03 05:31:46 -0700

    Fixing eslint errors

--- test/index.js
+++ test/index.js
@@ -5,20 +5,18 @@ var assert = require('assert'),
   imgCloud = require('../lib/index'),
   config = require('../config');
 
-var TEST_API_KEY = config.testApiKey;
-
 describe('img-cloud', function () {
-  
+ 
   describe('configure', function (){
-    it('should fail if no options are provided', function(){
+    it('should fail if no options are provided', function (){
       try {
         imgCloud.configure();
       } catch (e){
         assert(e.toString(), 'Missing configuration options');
       }
     });
-    
-    it('should fail if api key is provided', function(){
+
+    it('should fail if api key is provided', function (){
       try {
         imgCloud.configure({});
       } catch (e){
@@ -27,7 +25,7 @@ describe('img-cloud', function () {
     });
   });
   
-  describe('upload', function(){
+  describe('upload', function (){
     it('should upload image to img-cloud server', function () {
       var options = {
         folder: 'nodeModuleTest',
@@ -35,7 +33,7 @@ describe('img-cloud', function () {
       };
       
       imgCloud.configure({
-        apiKey: config.testApiKey 
+        apiKey: config.testApiKey
       });
       
       imgCloud.upload('test/fixtures/test.jpg', options, function (error, data){
@@ -49,32 +47,32 @@ describe('img-cloud', function () {
     });
     
     it('should fail if input file path not specified', function () {
-      imgCloud.upload('', {}, function (error, data){
+      imgCloud.upload('', {}, function (error){
         assert(error, '"path" is required');
       });
     });
     
     it('should fail if input file path is not a string', function () {
-      imgCloud.upload({}, function (error, data){
+      imgCloud.upload({}, function (error){
         assert(error, 'Invalid format for "path"');
       });
     });
     
     it('should fail if api key is not set', function () {
-      imgCloud.upload('test/fixtures/test.jpg', {}, function (error, data){
+      imgCloud.upload('test/fixtures/test.jpg', {}, function (error){
         assert(error, 'API Key not set');
       });
     });
     
     it('should fail if "tags" is not a string', function () {
-      imgCloud.upload('test/fixtures/test.jpg', {tags: ['test', 'node']}, function (error, data){
+      imgCloud.upload('test/fixtures/test.jpg', {tags: ['test', 'node']}, function (error){
         assert(error, 'Invalid format for "tags". A CSV of strings is expected.');
       });
     });
   });
   
-  describe('transform', function(){
-    it('should fail if path is not provided', function(){
+  describe('transform', function (){
+    it('should fail if path is not provided', function (){
       try {
         imgCloud.transform('', {});  
       } catch (e){
@@ -82,7 +80,7 @@ describe('img-cloud', function () {
       }
     });
     
-    it('should fail if height is not a number', function(){
+    it('should fail if height is not a number', function (){
       try {
         imgCloud.transform('/path/to/file', {height: 'abc'});  
       } catch (e){
@@ -90,7 +88,7 @@ describe('img-cloud', function () {
       }
     });
     
-    it('should fail if width is not a number', function(){
+    it('should fail if width is not a number', function (){
       try {
         imgCloud.transform('/path/to/file', {width: 'abc'});  
       } catch (e){
@@ -98,7 +96,7 @@ describe('img-cloud', function () {
       }
     });
     
-    it('should return the URL for the transformed image', function(){
+    it('should return the URL for the transformed image', function (){
       var imageTag = imgCloud.transform('icp_ca3a83/1441279211081_test.jpg', {
         width: 150, 
         height: 150,
@@ -117,7 +115,7 @@ describe('img-cloud', function () {
       assert(imageTag, expectedTag);
     });
     
-    it('should return the origninal URL if no transformation options are provided', function(){
+    it('should return the origninal URL if no transformation options are provided', function (){
       var imageTag = imgCloud.transform('icp_ca3a83/1441279211081_test.jpg');
       var expectedTag = '<img src="' + config.endPointBase + 'icp_ca3a83/1441279211081_test.jpg">';
       assert(imageTag, expectedTag);
